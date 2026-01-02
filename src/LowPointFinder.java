383718626f48b03a0b478f0bade840c51a0f4487
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2026-01-02 14:56:29 -0600

    Refactor LowPointFinder and implement printLowestPoint

--- src/LowPointFinder.java
+++ src/LowPointFinder.java
@@ -21,73 +21,160 @@ Implement printLowestPoint to correctly print the answer.
 
 */
 
-import java.util.Random;
+import java.util.*;
 
 public class LowPointFinder {
+
     public static class Map {
-        // do not change anything in the Map class unless you are constructing a specific test case
         private int mGrid[][] = null;
-        public int getNumRows() { return mGrid.length; }
-        public int getNumColumns() { return mGrid[0].length; }
+
+        public int getNumRows() {
+            return mGrid.length;
+        }
+
+        public int getNumColumns() {
+            return mGrid[0].length;
+        }
+
         public int getAltitude(int iRow, int iColumn) {
             return mGrid[iRow][iColumn];
         }
+
         public void printMap() {
             StringBuilder sbRow = new StringBuilder("    ");
             for (int i = 0; i < mGrid[0].length; i++) {
-                String strCell = String.format("%4s", "C"+i);
-                sbRow.append(strCell);
+                sbRow.append(String.format("%4s", "C" + i));
             }
             System.out.println(sbRow.toString());
+
             for (int i = 0; i < mGrid.length; i++) {
-                String strCell = String.format("%4s", "R"+i);
-                sbRow = new StringBuilder(strCell);
+                sbRow = new StringBuilder(String.format("%4s", "R" + i));
                 for (int j = 0; j < mGrid[0].length; j++) {
-                    strCell = String.format("%4d", getAltitude(i, j));
-                    sbRow.append(strCell);
+                    sbRow.append(String.format("%4d", getAltitude(i, j)));
                 }
                 System.out.println(sbRow.toString());
             }
         }
+
         private int changeAltitude(int iAltitude, Random random) {
             return iAltitude + random.nextInt(11) - 5;
         }
+
         public Map(int iNumRows, int iNumColumns, int iRandomSeed) {
             mGrid = new int[iNumRows][iNumColumns];
             Random random = new Random(iRandomSeed);
+
             for (int i = 0; i < iNumRows; i++) {
                 for (int j = 0; j < iNumColumns; j++) {
-                    int iAltitude = 0;
+                    int iAltitude;
                     if (i == 0) {
                         iAltitude = random.nextInt(101);
                     } else {
-                        int iSideAltitude = 0;
-                        if (j == 0) iSideAltitude = random.nextInt(101);
-                        else iSideAltitude = getAltitude(i, j-1);
-                        int iTopAltitude = getAltitude(i-1, j);
-                        iAltitude = (changeAltitude(iSideAltitude, random)
-                                + changeAltitude(iTopAltitude, random))/2;
+                        int side = (j == 0) ? random.nextInt(101) : getAltitude(i, j - 1);
+                        int top = getAltitude(i - 1, j);
+                        iAltitude = (changeAltitude(side, random) + changeAltitude(top, random)) / 2;
                         if (iAltitude < 0) iAltitude = 0;
-                        else if (iAltitude > 100) iAltitude = 100;
+                        if (iAltitude > 100) iAltitude = 100;
                     }
                     mGrid[i][j] = iAltitude;
                 }
             }
         }
     }
-    public static void printLowestPoint(Map map, int iRow, int iColumn) {
-        // implement this function (and any necessary helper code);
-        // replace the ??? with the correct information
-        System.out.println("The lowest reachable point occurs at "
-                +" ??? "+", "+" ??? "
-                +" with an altitude of "+" ??? ");
+
+    static int bestRow, bestCol, bestAlt;
+    static List<Integer> bestDrops;
+    static int R0, C0;
+
+    static int[] dr = {0, 0, -1, 1};
+    static int[] dc = {-1, 1, 0, 0};
+
+    static void grand_dfs(Map map, int r, int c, boolean[][] visited, List<Integer> drops) {
+        visited[r][c] = true;
+        int alt = map.getAltitude(r, c);
+        boolean moved = false;
+
+        for (int k = 0; k < 4; k++) {
+            int nr = r + dr[k];
+            int nc = c + dc[k];
+
+            if (nr < 0 || nc < 0 || nr >= map.getNumRows() || nc >= map.getNumColumns())
+                continue;
+            if (visited[nr][nc])
+                continue;
+
+            int nAlt = map.getAltitude(nr, nc);
+            if (nAlt <= alt) {
+                moved = true;
+                drops.add(alt - nAlt);
+                grand_dfs(map, nr, nc, visited, drops);
+                drops.remove(drops.size() - 1);
+            }
+        }
+
+        if (!moved) {
+            boolean better = false;
+
+            if (alt < bestAlt) {
+                better = true;
+            } else if (alt == bestAlt) {
+                int n = Math.min(drops.size(), bestDrops.size());
+                for (int i = 0; i < n; i++) {
+                    if (!drops.get(i).equals(bestDrops.get(i))) {
+                        better = drops.get(i) > bestDrops.get(i);
+                        break;
+                    }
+                }
+
+                if (!better && drops.size() == bestDrops.size()) {
+                    int d1 = Math.abs(r - R0) + Math.abs(c - C0);
+                    int d2 = Math.abs(bestRow - R0) + Math.abs(bestCol - C0);
+                    if (d1 < d2) better = true;
+                }
+            }
+
+            if (better) {
+                bestAlt = alt;
+                bestRow = r;
+                bestCol = c;
+                bestDrops = new ArrayList<>(drops);
+            }
+        }
+
+        visited[r][c] = false;
     }
 
-    // usage: java LowPointFinder [row] [column]
-    //    for example: java LowPointFinder 1 9
-    public static void main(String args[]) {
+    public static void printLowestPoint(Map map, int iRow, int iColumn) {
+        R0 = iRow;
+        C0 = iColumn;
+
+        bestAlt = map.getAltitude(iRow, iColumn);
+        bestRow = iRow;
+        bestCol = iColumn;
+        bestDrops = new ArrayList<>();
+
+        boolean[][] visited = new boolean[map.getNumRows()][map.getNumColumns()];
+        grand_dfs(map, iRow, iColumn, visited, new ArrayList<>());
+
+        System.out.println(
+                "The lowest reachable point occurs at R" +
+                        bestRow + ", C" + bestCol +
+                        " with an altitude of " + bestAlt
+        );
+    }
+
+    public static void main(String[] args) {
         Map map = new Map(10, 10, 0);
         map.printMap();
-        printLowestPoint(map, Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+
+        int row = 0;
+        int col = 0;
+
+        if (args.length >= 2) {
+            row = Integer.parseInt(args[0]);
+            col = Integer.parseInt(args[1]);
+        }
+
+        printLowestPoint(map, row, col);
     }
 }
